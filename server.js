da90b6b5b4fdecfc687d92ca43a909bc9062708b
asharron <alexsharron@gmail.com>
2019-01-30 18:51:01 -0500

    Added missing parenthese

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const path = require('path');
 //Allows it to be able to parse json from front-end requests
 app.use(bodyParser.json());
 
+//Static assets for React
+app.use(express.static(path.join(__dirname, "/dist")));
 //Serves up video files based on url
 app.use("/videos", express.static(path.join(__dirname, "/videos")));
 
 //Serves up the built React files on every route
 app.all("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "/dist/index.html");
+	res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 //Start the server
