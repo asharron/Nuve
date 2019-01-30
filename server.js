e30808e22c35be4a67a865d9bd0c2ce6983c10c4
asharron <alexsharron@gmail.com>
2019-01-30 18:47:44 -0500

    Cleaned up server code and always serves up react

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const rp = require('request-promise-native');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-const fs = require('fs');
-const guessit = require('guessit-exec');
-
-const tmdbconfig = require('./tmdb.json');
-const tmdbKey = process.env.TMDB_API_KEY;
-const movieSearch = "https://api.themoviedb.org/3/search/movie";
-const imageUrl = tmdbconfig.images.secure_base_url;
 
 //Allows it to be able to parse json from front-end requests
 app.use(bodyParser.json());
 
-//Serves up the built React files
-app.use(express.static(path.join(__dirname, '/dist')));
 //Serves up video files based on url
 app.use("/videos", express.static(path.join(__dirname, "/videos")));
+
+//Serves up the built React files on every route
+app.all("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/dist/index.html");
+});
 
 //Start the server
 app.listen(8080, () => {
