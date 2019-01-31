c6bc0849a2090b3d727794e070eb89acfa630f74
asharron <alexsharron@gmail.com>
2019-01-30 21:20:32 -0500

    Remapped api routes to /api/

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.static(path.join(__dirname, "/dist")));
 app.use("/videos", express.static(path.join(__dirname, "/videos")));
 
 //Returns JSON of video info for one particular movie
-app.get('/videoinfo', (req, res) => {
+app.get('/api/videoinfo', (req, res) => {
 	data = metamap[req.query.id] || { error: "That id could not be found" };
 	res.send(data);
 });
 
 
 //Returns JSON of video info for number of videos specfied
-app.get('/videos', (req, res) => {
+app.get('/api/videos', (req, res) => {
 	data = {};
 	var start = req.query.start || 0;
 	var end = req.query.end || 10;
